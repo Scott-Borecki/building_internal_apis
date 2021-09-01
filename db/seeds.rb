c6d38837b3cf64249582ad165f7db59ef53b710b
Scott Borecki <scottborecki@gmail.com>
2021-09-01 12:50:01 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'factory_bot'
+
+include FactoryBot::Syntax::Methods
+
+create_list(:book, 10)
