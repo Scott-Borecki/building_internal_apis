fa8251287bc990708dd05bae21c58192368c10ef
Scott Borecki <scottborecki@gmail.com>
2021-09-01 12:49:19 -0600

    Initial setup

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :show, :create, :update, :destroy]
+    end
+
+    namespace :v2 do
+      resources :books, only: [:index]
+    end
+  end
+end
